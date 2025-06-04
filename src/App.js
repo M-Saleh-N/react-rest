91072448b98509823c46cea11d585053e936fbd8
M-Saleh-N <salehnasser65784@gmail.com>
2025-06-04 23:16:16 +0300

    added contents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Add Task</h1>
+      <AddTaskComponents/>
+      <ViewTaskComponents/>
     </div>
   );
+}
+const AddTaskComponents = () => {
+  const [task, setTask] = useState("");
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  }
+  const handleClick = async () => {
+    try {
+      const response = await axios.post("http://127.0.0.1:5000/todos",{
+        task : task
+      })
+      if(response.status === 201){
+        alert("Task Created")
+      }
+    }catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <div>
+      <input onChange={handleChange} type="text" placeholder="Add task..."/>
+      <button onClick={handleClick}>Add Task</button>
+    </div>
+  )
+}
+
+const ViewTaskComponents = () => {
+  const [task, setTask] = useState([])
+
+  const fetchTask = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/todos")
+      if(response.status === 200){
+        setTask(response.data)
+      }
+    }catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() =>{
+    fetchTask()
+  }, [task.length])
+  return (
+    <div>
+      <h4>Task</h4>
+      {Object.values(task).map((val, i) => <p key={i}>{val}</p>)}
+    </div>
+  )
+}
+
+const UpdateTaskCompnent = () => {
+  return(
+    <div>
+      <input type="text" placeholder="Update Task"/>
+      <button>Update Task</button>
+    </div>
+  )
 }
 
 export default App;
